6c95b15658a5273bf49d00a80b086a95781dd1cf
limochen <2320035651@qq.com>
2022-11-17 17:50:50 +0800

    make grids

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './home.scss';
+import headerBg from '../images/header001.png';
+
+
+
+
 
 export const Home = () => {
   return (
-    <div>
-      你好
+  <div className="home">
+      <header style={{backgroundImage: `url(${headerBg})`}}/>
+      <main>
+        <section className="section1"></section>
+        <section className="section2"></section>
+        <section className="section3"></section>
+        <section className="section4"></section>
+        <section className="section5"></section>
+      </main>
     </div>
   );
 };
