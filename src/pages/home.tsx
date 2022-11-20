3b008f5fdcef34dc88fcb8f8f091d15de06255f6
limochen <2320035651@qq.com>
2022-11-20 13:30:26 +0800

    重构chart1

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useEffect,useRef } from 'react';
 import './home.scss';
 import headerBg from '../images/header001.png';
-
-
-
+import { Chart1 } from './../components/chart-1';
 
 
 export const Home = () => {
+ 
   return (
   <div className="home">
       <header style={{backgroundImage: `url(${headerBg})`}}/>
       <main>
-        <section className="section1"></section>
-        <section className="section2"></section>
-        <section className="section3"></section>
-        <section className="section4"></section>
-        <section className="section5"></section>
+        <section className="section1">
+        <Chart1 /> 
+         
+        </section>
+        <section className="bordered section2"></section>
+        <section className="bordered section3"></section>
+        <section className="bordered section4"></section>
+        <section className="bordered section5"></section>
       </main>
     </div>
   );
